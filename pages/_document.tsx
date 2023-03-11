8923b32f10e8fa523252d946cc90bc6eca643f1c
baseplate77 <base8087@gmail.com>
2023-03-12 00:16:37 +0530

    initital commit

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,23 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <script
+          async
+          src="https://reflio.com/js/reflio.min.js"
+          data-reflio="k6da5efnjz88obh"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
+        <script type="text/javascript">
+          await Reflio.signup('yourcustomer@email.com')
+        </script>
       </body>
     </Html>
-  )
+  );
 }
